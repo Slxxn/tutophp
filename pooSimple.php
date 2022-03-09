1b3a6a2efb08f565107f39b3552ec0bb8f575f8e
Slxxn <100938472+Slxxn@users.noreply.github.com>
2022-03-09 12:22:20 -0400

    tutoPhp v.002

---
+++ pooSimple.php
@@ -0,0 +1,11 @@
+<?php
+
+    class Simple {
+
+       private $nomprive = "totot";
+       public $nomPublique ="Gulliano"; 
+
+    }
+
+
+?>
---
+++ usePooSimple.php
@@ -0,0 +1,9 @@
+<?php
+
+    include("pooSimple.php") ;
+
+    $simpleTest = new Simple() ;
+
+    echo $simpleTest->nomPublique ;
+
+?>
